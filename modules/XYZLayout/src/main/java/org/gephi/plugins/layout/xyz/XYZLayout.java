642be7c819f9b39afc896b98e648d98d7eb549e2
jafranc <jacquesn7@gmail.com>
2020-06-25 15:25:07 -0700

    minor change to rotate view

--- modules/XYZLayout/src/main/java/org/gephi/plugins/layout/xyz/XYZLayout.java
+++ modules/XYZLayout/src/main/java/org/gephi/plugins/layout/xyz/XYZLayout.java
@@ -68,12 +68,17 @@ public class XYZLayout implements Layout {
     private boolean centered = true;
     private Column coordX;
     private Column coordY;
+    private Column coordZ;
+    private final int loopingDelay = 100;//ms
+    private static int loopCounter = 0;
+    
     private String projection = "OrthoX";
     public static String[] rows = {
         "OrthoX",
         "OrthoY",
         "OrthoZ"
     };
+
 
     public XYZLayout(XYZLayoutBuilder builder) {
         this.builder = builder;
@@ -94,6 +99,11 @@ public class XYZLayout implements Layout {
                             || c.getTitle().equalsIgnoreCase("coordY")
                             || c.getTitle().equalsIgnoreCase("Y")) {
                         coordY = c;
+                    } else if (c.getId().equalsIgnoreCase("coordZ")
+                            || c.getId().equalsIgnoreCase("Z")
+                            || c.getTitle().equalsIgnoreCase("coordZ")
+                            || c.getTitle().equalsIgnoreCase("Z")) {
+                        coordZ = c;
                     }
                 }
             }
@@ -107,7 +117,7 @@ public class XYZLayout implements Layout {
 
     @Override
     public void goAlgo() {
-        double cX, cY;
+        double cX, cY, cZ;
         float nodeX, nodeY;
         float averageX = 0;
         float averageY = 0;
@@ -127,8 +137,9 @@ public class XYZLayout implements Layout {
             }
         }
 
+	double theta = 2*Math.PI/10*loopCounter;	
         // Mercantor
-        if (projection.equals("OrthoX")) {
+        if (projection.equals("OrthoZ")) {
             
             //apply the formula:
             for (Node n : validNodes) {
@@ -137,12 +148,37 @@ public class XYZLayout implements Layout {
                 }
                 cX = getNodeCoordX(graph, n);
                 cY = getNodeCoordY(graph, n);
-               
-                nodeX = (float) cX;
-                nodeY = (float) cY;
+              
+                nodeX = (float) cX;//(cX*Math.cos(theta) - cY*Math.sin(theta));
+                nodeY = (float) cY;//(cX*Math.sin(theta) + cY*Math.cos(theta));
 
+		
                 averageX += nodeX;
                 averageY += nodeY;
+		
+                n.setX(nodeX);
+                n.setY(nodeY);
+            }
+
+            averageX = averageX / validNodes.size();
+            averageY = averageY / validNodes.size();
+        } //
+        else if (projection.equals("OrthoY")) {
+            
+            //apply the formula:
+            for (Node n : validNodes) {
+                if (n.getLayoutData() == null || !(n.getLayoutData() instanceof XYZLayoutData)) {
+                    n.setLayoutData(new XYZLayoutData());
+                }
+                cX = getNodeCoordX(graph, n);
+                cZ = getNodeCoordZ(graph, n);
+               
+                nodeX = (float) cX;//(cX*Math.cos(theta) - cZ*Math.sin(theta));
+               	nodeY = (float) cZ;//(cX*Math.sin(theta) + cZ*Math.cos(theta));
+
+
+                averageX += nodeX;
+               	averageY += nodeY;
 
                 n.setX(nodeX);
                 n.setY(nodeY);
@@ -150,11 +186,32 @@ public class XYZLayout implements Layout {
 
             averageX = averageX / validNodes.size();
             averageY = averageY / validNodes.size();
-        } // Transverse Mercantor
-        else if (projection.equals("OrthoY")) {
         }
-        else if (projection.equals("OrthoZ")) {
-        }
+        else if (projection.equals("OrthoX")) {
+            
+            //apply the formula:
+            for (Node n : validNodes) {
+                if (n.getLayoutData() == null || !(n.getLayoutData() instanceof XYZLayoutData)) {
+                    n.setLayoutData(new XYZLayoutData());
+                }
+
+		cY = getNodeCoordY(graph, n);
+                cZ = getNodeCoordZ(graph, n);
+               
+                nodeX = (float) cY;//(cY*Math.cos(theta) - cZ*Math.sin(theta));
+                nodeY = (float) cZ;//(cY*Math.sin(theta) + cZ*Math.cos(theta));
+
+	   
+                averageX += nodeX;
+                averageY += nodeY;
+	
+                n.setX(nodeX);
+                n.setY(nodeY);
+            }
+
+            averageX = averageX / validNodes.size();
+            averageY = averageY / validNodes.size();
+	}
 
        
         //recenter the graph
@@ -169,7 +226,17 @@ public class XYZLayout implements Layout {
         }
 
         graph.readUnlock();
-	done = true;
+
+	if(++loopCounter>10)
+		done = true;
+
+	try{
+		Thread.sleep(loopingDelay);
+	}
+	catch( InterruptedException ex )
+	{
+		Exceptions.printStackTrace(ex);
+	}
 
        
     }
@@ -184,13 +251,18 @@ public class XYZLayout implements Layout {
         return lon.doubleValue();
     }
 
+    private double getNodeCoordZ(Graph graph, Node n) {
+        Number lon = (Number) n.getAttribute(coordZ, graph.getView());
+        return lon.doubleValue();
+    }
+    
     @Override
     public void endAlgo() {
     }
 
     @Override
     public boolean canAlgo() {
-        return !done && coordX != null && coordY != null;
+        return !done && coordX != null && coordY != null && coordZ != null;
     }
 
     @Override
@@ -211,6 +283,12 @@ public class XYZLayout implements Layout {
                     XYZLAYOUT,
                     NbBundle.getMessage(XYZLayout.class, "XYZLayout.coordY.desc"),
                     "getCoordY", "setCoordY", NodeColumnAllNumbersEditor.class));
+            properties.add(LayoutProperty.createProperty(
+                    this, Column.class,
+                    NbBundle.getMessage(XYZLayout.class, "XYZLayout.coordZ.name"),
+                    XYZLAYOUT,
+                    NbBundle.getMessage(XYZLayout.class, "XYZLayout.coordZ.desc"),
+                    "getCoordZ", "setCoordZ", NodeColumnAllNumbersEditor.class));
             properties.add(LayoutProperty.createProperty(
                     this, String.class,
                     NbBundle.getMessage(XYZLayout.class, "XYZLayout.projection.name"),
@@ -274,10 +352,19 @@ public class XYZLayout implements Layout {
         this.coordY = coordY;
     }
 
+    public Column getCoordZ() {
+	return coordZ;
+    }
+
+    public void setCoordZ(Column coordZ) {
+        this.coordZ = coordZ;
+    }
+
     private static class XYZLayoutData implements LayoutData {
 
         //Data
         public double x = 0f;
         public double y = 0f;
+	public double z = 0f;
     }
 }
